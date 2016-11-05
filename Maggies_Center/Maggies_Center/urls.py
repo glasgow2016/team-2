92fc617003e7eb9ee14499e0717da24432e2afba
Tom Parker <tjparker@hotmail.co.uk>
2016-11-05 19:42:58 +0000

    Partial export

--- Maggies_Center/Maggies_Center/urls.py
+++ Maggies_Center/Maggies_Center/urls.py
@@ -19,5 +19,6 @@ from maggies_webapp import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.main_page, name='index')
+    url(r'^$', views.main_page, name='index'),
+    url(r'^export/', views.export, name='export')
 ]
--- Maggies_Center/maggies_webapp/views.py
+++ Maggies_Center/maggies_webapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
+from maggies_webapp.models import Visit
 # Create your views here.
 
 
@@ -9,3 +10,12 @@ def login_page(request):
 
 def main_page(request):
     return render(request,'main_page.html')
+
+
+def export(request):
+    if (request.method) == 'GET':
+        print("Received export request")
+        print(request.GET.get('startdate'), request.GET.get('enddate'))
+        visits = Visit.objects.all()
+        print(visits)
+
