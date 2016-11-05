a4ffbf85bc96598a75a2277822436ff0d9d7bae5
Pablo Arteaga <pablo@pabloarteaga.es>
2016-11-05 22:42:48 +0000

    Finished registration

--- Maggies_Center/maggies_webapp/urls.py
+++ Maggies_Center/maggies_webapp/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import include, url
-from .views import Export, schedule
+from .views import Export, schedule, AddUser
 
 urlpatterns = [
         url(r'^accounts/', include('registration.backends.default.urls')),
         url(r'^export/', Export.as_view(), name='export'),
         url(r'^schedule/', schedule, name='schedule'),
+        url(r'^add_user/', AddUser.as_view(), name="add_user")
     ]
