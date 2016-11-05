5a532f4a7bcced4a6bd3ec251a263e773fa963d2
Alistair Miles <ac.counter@hotmail.com>
2016-11-05 20:18:01 +0000

    work on the schedule form

--- Maggies_Center/maggies_webapp/views.py
+++ Maggies_Center/maggies_webapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from maggies_webapp import models
+from django.contrib.auth import get_user
 # Create your views here.
 
 
@@ -13,7 +14,15 @@ def main_page(request):
 
 def schedule(request):
     context_dict = {}
+    current_user = models.StaffMember.objects.get(user_mapping = get_user(request))
     context_dict["schedule"] = {}
-    models.Activity.objects.filter()
+    for activity in models.Activity.objects.filter(centre=current_user.centre):
+        for day in activity.scheduled_times_array:
+            context_dict["schedule"][day] = {}
+            context_dict["schedule"][day][activity.id] = []
+            for time in activity.scheduled_times_array[day]:
+                context_dict["schedule"][day][activity.id].append(time)
+
+    models.Activity.objects.filter(centre="blah")
     return render(request,'schedule.html',context_dict)
 
