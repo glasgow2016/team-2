b871692bc9464e96dece0e5f2b5c4ab45411993f
AigarsReiters <areiters95@gmail.com>
2016-11-05 15:17:28 +0000

    aws script using current folder for the .pem file by default

--- AWS/awsteam2.sh
+++ AWS/awsteam2.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-if [ -z ${TEAM2KEYLOCATION+x} ]; then echo "Please export TEAM2KEYLOCATION=/some/dir/file.pem"; else
-ssh -i $TEAM2KEYLOCATION force@ec2-52-18-198-15.eu-west-1.compute.amazonaws.com
+if [ -z ${TEAM2KEYLOCATION+x} ]; then 
+echo "Please export TEAM2KEYLOCATION=/some/dir/file.pem\nUsing default";
+TEAM2KEYLOCATION=./glasgow_team_2.pem
 fi
+ssh -i $TEAM2KEYLOCATION force@ec2-52-18-198-15.eu-west-1.compute.amazonaws.com
+
