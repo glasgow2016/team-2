0993b18a285522e645ff55a506d1dba115486dae
Force <force@ip-172-31-4-94.eu-west-1.compute.internal>
2016-11-05 18:54:21 +0000

    continuous int script

---
+++ autorun.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+localPATH=~/team-2/                          # path of current directory
+sep='---------------'
+
+if git pull | grep -q 'unpacking' || true ; then
+    echo Updated
+    kill -9 $(pidof python)
+    (python ~/team-2/Maggies_Center/manage.py runserver 0.0.0.0:8000 &>/dev/null) &disown
+    echo Finished!
+else
+    echo Not updated
+fi
+
