6c13c6466885b0586cc6aec6daa46b57972ea31f
Pablo Arteaga <pablo@pabloarteaga.es>
2016-11-05 20:13:43 +0000

    Updated files

--- Maggies_Center/maggies_webapp/views.py
+++ Maggies_Center/maggies_webapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 
+from .forms import BaseUserForm, NewStaffForm
 
 def login_page(request):
     html ='login page html'
@@ -9,3 +11,13 @@ def login_page(request):
 
 def main_page(request):
     return render(request,'main_page.html')
+
+class AddUser(View, LoginRequiredMixin):
+
+    def get(self, request):
+        form_a = BaseUserForm()
+        form_b = NewStaffForm()
+        return render("templates/new_")
+
+    def post(self, request):
+        pass
