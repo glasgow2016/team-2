3a4731a035cd33baa3bb34a94e121ac150f6ed42
Force <force@ip-172-31-4-94.eu-west-1.compute.internal>
2016-11-05 19:06:27 +0000

    Working CI

--- autorun.sh
+++ autorun.sh
@@ -2,7 +2,7 @@
 localPATH=~/team-2/                          # path of current directory
 sep='---------------'
 
-if git pull | grep -q 'unpacking' || true ; then
+if git pull | grep -q 'unpacking' ; then
     echo Updated
     kill -9 $(pidof python)
     (python ~/team-2/Maggies_Center/manage.py runserver 0.0.0.0:8000 &>/dev/null) &disown
