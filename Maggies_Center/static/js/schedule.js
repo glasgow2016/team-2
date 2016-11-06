a4e7a4da1cb2237ae32a111d3cebf4432f5a631a
Alistair Miles <ac.counter@hotmail.com>
2016-11-06 11:48:14 +0000

    filter button on main page is complete. last commit #MakeCodeGreatAgain

--- Maggies_Center/static/js/schedule.js
+++ Maggies_Center/static/js/schedule.js
@@ -5,15 +5,22 @@ $(document).ready(function () {
     });
 
     var row_count = 1;
-    $("#add-row").click(function(){
+    $("#add-row").click(function () {
         var $button = $("#add-row");
         var $original = $("#last-row").attr("id", "whocares");
         var $clone = $original.clone(true);
-        $clone.attr("id","last-row");
+        $clone.attr("id", "last-row");
         $clone.insertBefore($original);
         $button.remove();
         row_count++;
     });
 
+    $("#filter_switch").change(function () {
+        var switch_state = $(this).context.checked;
+        if (!switch_state)
+            $("div[data-in-bld='False']").fadeOut();
+        else
+            $("div[data-in-bld='False']").fadeIn();
+    });
 });
 
