645a816881b8447f276eab5894794605abeaf63f
Pablo Arteaga <pablo@pabloarteaga.es>
2016-11-06 10:10:03 +0000

    Premerge

--- Maggies_Center/static/js/main.js
+++ Maggies_Center/static/js/main.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
     $('select').material_select();
 	$(".button-collapse").sideNav();
-	Materialize.updateTextFields();#
+	Materialize.updateTextFields();
+	$("#centre_select").change(function() {
+	    window.location.href=window.location.href.split("?")[0] + "?centre=" + $(this).val();
+	});
 });
 function addVisitorCard() {
 	alert("woo");
