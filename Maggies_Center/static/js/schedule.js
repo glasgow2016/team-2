ec4150714911b51d9857b30e6723462f1854d357
Alistair Miles <ac.counter@hotmail.com>
2016-11-06 00:00:50 +0000

    Can now add rows to the schedule

--- Maggies_Center/static/js/schedule.js
+++ Maggies_Center/static/js/schedule.js
@@ -4,8 +4,12 @@ $(document).ready(function () {
         selectYears: 15 // Creates a dropdown of 15 years to control year
     });
     $('select').material_select();
+    var row_count = 1;
     $("#add-row").click(function(){
-
+        $original = $('#last-row').attr("id","who-cares");
+        $clone = $original.clone(true,true).insertBefore($original);
+        $clone.attr("id","last-row");
+        row_count++;
     });
 });
 
