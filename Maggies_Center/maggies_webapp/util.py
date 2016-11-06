2dce6c2484007d4918312738dcb04d0594735b42
Pablo Arteaga <pablo@pabloarteaga.es>
2016-11-06 07:39:20 +0000

    Added entry edition

--- Maggies_Center/maggies_webapp/util.py
+++ Maggies_Center/maggies_webapp/util.py
@@ -1,3 +1,5 @@
+from maggies_webapp.models import Centre, StaffMember
+
 
 class Util(object):
     @staticmethod
@@ -6,5 +8,17 @@ class Util(object):
                 'name': visitor.visitor_name,
                 'gender': visitor.related_visit.get_gender_display(),
                 'cancer_type': visitor.related_visit.get_cancer_site_display(),
-                'id': visitor.id
-            }
+                'id': visitor.pk
+         }
+
+    @staticmethod
+    def check_user_can_access(staffmember, visit):
+        if staffmember.staff_type == "AD":
+            return True
+        centers = Centre.objects.filter(staffmember=staffmember)
+        return visit.visit_site in centers
+
+    @staticmethod
+    def check_user_obj_can_access(user, visit):
+        userprofile = StaffMember.objects.get(user_mapping=user)
+        return Util.check_user_can_access(userprofile, visit)
