afd32830e50fe918ea79e38665fed85fbacd5cf2
Tom Parker <tjparker@hotmail.co.uk>
2016-11-06 11:50:42 +0000

    update ajax

--- Maggies_Center/static/js/inbuilding.js
+++ Maggies_Center/static/js/inbuilding.js
@@ -5,7 +5,7 @@
 
 function visitor_in(mapping_id) {
     $.ajax({
-        url: "/async/set-left/" + mapping_id,
+        url: "/async/set-left/" + mapping_id + "/",
         context: document.body,
         success: function () {
             $(this).addClass("done");
